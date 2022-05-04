a38419ac9f0795f0619ad5a97314c494320607d7
Lucas Araújo <lucasarasouza@hotmail.com>
2022-05-04 15:53:57 -0300

    Adiciona o 'backend.tf' e a criação de subnets para o hub com for_each

--- vnet-hub-and-spoke-module/main.tf
+++ vnet-hub-and-spoke-module/main.tf
@@ -1,14 +1,20 @@
 resource "azurerm_resource_group" "rg" {
-    name = var.resource_group_name
+  name     = var.resource_group_name
   location = var.location
 }
 
-resource "azurerm_virtual_network" "vnet-hub" {
-    name = var.vnet_hub_name
-    resource_group_name = azurerm_resource_group.rg.name
-    location = azurerm_resource_group.rg.location
-    address_space = var.hub_address_space
-    depends_on = [data.azurerm_resource_group.rg-vnet]
-    tags = var.tags
+resource "azurerm_virtual_network" "vnet_hub" {
+  name                = var.vnet_hub_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  address_space       = var.hub_address_space
+  tags                = var.tags
 }
 
+resource "azurerm_subnet" "subnets_hub" {
+  for_each             = var.subnets_hub
+  name                 = each.key
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet_hub.name
+  address_prefixes     = each.value.address_prefixes
+}
--- vnet-hub-and-spoke-module/variables.tf
+++ vnet-hub-and-spoke-module/variables.tf
@@ -9,28 +9,34 @@ variable "resource_group_name" {
 }
 
 variable "vnet_hub_name" {
-  type = string
+  type        = string
   description = "Nome da Virtual Network hub"
 }
 
 variable "hub_address_space" {
-  type = list(string)
+  type        = list(string)
   description = "Address Space da VNet Hub"
+  default     = ["10.0.0.0/16"]
 }
 
 variable "tags" {
   description = "Tags que serão atribuídas aos recursos criados pelo módulo"
-  type = map(string)
+  type        = map(string)
   default = {
     "environment" = "test"
-    "project" = "hub-and-spoke-vnet-module"
+    "project"     = "hub-and-spoke-vnet-module"
   }
 }
 
-variable "subnets-hub" {
-  type = any
+variable "subnets_hub" {
+  type        = any
   description = "Subnets da VNet Hub"
   default = {
-      
+    subnet01 = {
+      address_prefixes = ["10.0.1.0/24"]
+    }
+    subnet02 = {
+      address_prefixes = ["10.0.2.0/24"]
+    }
   }
 }
