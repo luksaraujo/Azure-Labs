abf03f9cecac8d354642bebc1bb0a3f7e287eb88
Lucas Araújo <lucasarasouza@hotmail.com>
2022-05-06 14:29:10 -0300

    Adição dos parâmetros necessários para a criação das demais VNets e Subnets na chamada do módulo

--- vnet-hub-and-spoke-module/main.tf
+++ vnet-hub-and-spoke-module/main.tf
@@ -15,6 +15,32 @@ module "hub-and-spoke" {
           address_prefixes = "10.0.2.0/24"
         }
       ]
+    },
+    spoke_vnet_01 = {
+      address_space = ["10.1.0.0/16"]
+      subnet = [
+        {
+          name             = "spoke_subnet_01"
+          address_prefixes = "10.1.1.0/24"
+        },
+        {
+          name             = "spoke_subnet_02"
+          address_prefixes = "10.1.2.0/24"
+        }
+      ]
+    },
+    spoke_vnet_02 = {
+      address_space = ["10.2.0.0/16"]
+      subnet = [
+        {
+          name             = "spoke_subnet_01"
+          address_prefixes = "10.2.1.0/24"
+        },
+        {
+          name             = "spoke_subnet_02"
+          address_prefixes = "10.2.2.0/24"
+        }
+      ]
     }
   }
 }
